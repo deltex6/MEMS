c65aeeaa15a656097c1207428b236455ab8fca70
Tomasz Persa <tomasz.stanislaw.persa@gmail.com>
2025-11-24 04:05:12 +0100

    Dodaj pliki projektów.

---
+++ MedicalEquipmentManagementSystem/MedicalEquipmentManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MedicalEquipmentManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
